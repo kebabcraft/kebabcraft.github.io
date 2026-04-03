99e389ea4ae3be1ca0c70e20fbc04639b1d25de6
NoName-Coding <natur2.stadion@icloud.com>
2026-04-03 16:16:25 +0200

    removed relativity

--- panorama.js
+++ panorama.js
@@ -22,7 +22,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Load your panorama cube
   const loader = new THREE.CubeTextureLoader();
-  loader.setPath('./src/img/panorama/');
+  loader.setPath('/src/img/panorama/');
   const texture = loader.load([
     'panorama_1.png', // +X (right)
     'panorama_3.png', // -X (left)
